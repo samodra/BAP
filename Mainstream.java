cd7d2a8b63529b54f30d2808435c7dce6798ac07
samodra <eka.samod@gmail.com>
2015-04-29 07:44:42 +0700

    Create Mainstream.java

---
+++ Mainstream.java
@@ -0,0 +1,25 @@
+# BAP
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainstream;
+
+/**
+ *
+ * @author L855-S5309
+ */
+public class Mainstream {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginAs ls = new LoginAs();
+        ls.setTitle("Login As");
+        ls.setVisible(true);
+    }
+
+}
